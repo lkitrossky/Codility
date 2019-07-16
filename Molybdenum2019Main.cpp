c669ff07257a27b012ab4a3fddc7440bfb41016d
Levi KItrossky <lkitrossky@gmail.com>
2019-07-16 10:03:34 +0300

    Update Molybdenum2019Main.cpp
    vector instead of map, also gold

--- Molybdenum2019Main.cpp
+++ Molybdenum2019Main.cpp
@@ -7,44 +7,53 @@
 
 using namespace std;
 #include <vector>
-#include <map>
+//#include <map>
 #include <set>
 #include <algorithm>
 vector<int> solution(int K, int M, vector<int> &A) {
 	int N = A.size();
 	int leader = N / 2 + 1;
-	map<int, int> current; 
-	// Damian Werpachowski noted that this may result in O(M*log (M)) and not O(M). 
-	//If map is replaced by vector it will be O(N+M), but for sparce array and small numbers it maybe even worse
+	//map<int, int> current; // Damian Werpachowski noted that this may result in O(M*log (M)) and not O(M). If map is replaced by vector it will be O(N+M), but for sparce array and small numbers it maybe even worse
+	//New version with vector:  https://app.codility.com/cert/view/certRZ3FSF-4NBKB4YKYWUX2HK8/details/   also gold
+	vector<int> current(M+2, 0); 
 	vector<int> res;
 	set<int> resSet;
 //initialization
 	for (int i = 0; i < K; i++) {
+		/*
 		if (current.find(A.at(i) + 1) == current.end())
 			current.insert(pair<int, int>(A.at(i) + 1, 1));
 		else
 			current.at(A.at(i) + 1)++;
+			*/
+		current.at(A.at(i) + 1)++;
 		if (current.at(A.at(i) + 1) >= leader)
 			resSet.insert(A.at(i) + 1);
 	}
 	for (int i = K; i < N; i++) {
+		/*
 		if (current.find(A.at(i) ) == current.end())
 			current.insert(pair<int, int>(A.at(i) , 1));
 		else
 			current.at(A.at(i) )++;
+		*/
+		current.at(A.at(i))++;
 		if (current.at(A.at(i) ) >= leader)
 			resSet.insert(A.at(i) );
 	}
 //movement
 	for (int i = K; i < N; i++) {
 		current.at(A.at(i))--;
+		/*
 		if (current.find(A.at(i) + 1) == current.end())
 			current.insert(pair<int,int>(A.at(i) + 1, 1));
 		else
-			current.at(A.at(i)+1)++;	
-		if (current.find(A.at(i - K)) == current.end())
+			current.at(A.at(i)+1)++;
+			*/
+		current.at(A.at(i) + 1)++;
+		/*if (current.find(A.at(i - K)) == current.end())
 			current.insert(pair<int, int>(A.at(i - K), 1));
-		else
+		else */
 			current.at(A.at(i - K))++;
 		current.at(A.at(i-K) + 1)--;
 		if (current.at(A.at(i) + 1) >= leader)
